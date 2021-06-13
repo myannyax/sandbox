e08efa1d5fc39dad51b3435cc31a6967b8e334bb
Nikita Tsarev <nikts53@gmail.com>
2021-06-13 21:51:16 +0300

    fix

--- test/test_all.sh
+++ test/test_all.sh
@@ -7,7 +7,7 @@ pushd "$(dirname "$0")" >/dev/null
 "./test.sh" memory_limit memory_limit_child niceness \
             niceness \
             permissions_read permissions_write \
-            signals signal_child \
+            signals signals_child \
             timeout timeout_child
 
 popd > /dev/null
