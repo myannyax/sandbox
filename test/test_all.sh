5040a6aa7dbde937b76cce21523760d6f6c66f8c
Maria Malysheva <mn.malysheva@mail.ru>
2021-06-13 21:48:02 +0300

    fix

--- test/test_all.sh
+++ test/test_all.sh
@@ -7,7 +7,7 @@ pushd "$(dirname "$0")" >/dev/null
 "./test.sh" memory_limit memory_limit_child niceness \
             niceness \
             permissions_read permissions_write \
-            signal signal_child \
+            signals signal_child \
             timeout timeout_child
 
 popd > /dev/null
