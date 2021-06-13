caffe0ed4cbb892297c746e1fff730f25a788350
Maria Malysheva <mn.malysheva@mail.ru>
2021-06-13 15:32:25 +0300

    do not log process id

--- src/modules/priority.cpp
+++ src/modules/priority.cpp
@@ -13,8 +13,8 @@ void PriorityModule::apply(Runner &runner) {
         int niceness = niceness_opt.value();
         if (setpriority(PRIO_PROCESS, pid, niceness) != 0) {
             MultiprocessLog::log_error(
-                "Failed to set niceness of process " + std::to_string(pid)
-                + " to " + std::to_string(niceness) + ": " + std::strerror(errno));
+                "Failed to set niceness to " + std::to_string(niceness) + ": " + std::strerror(errno)
+            );
         }
     });
 }
