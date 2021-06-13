a57a3bd59ffcf7db765473337297a751ae977238
Nikita Tsarev <nikts53@gmail.com>
2021-06-13 21:21:31 +0300

    fork test

--- test/test_all.sh
+++ test/test_all.sh
@@ -4,6 +4,6 @@ set -e
 
 pushd "$(dirname "$0")" >/dev/null
 
-"./test.sh" memory_limit permissions_read permissions_write
+"./test.sh" memory_limit permissions_read permissions_write fork
 
 popd > /dev/null
