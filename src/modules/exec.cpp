d6d74207769c9dd38f399c2780d820ade6da6bfa
Nikita Tsarev <nikts53@gmail.com>
2021-06-13 12:33:15 +0300

    drop capabilities

--- src/modules/exec.cpp
+++ src/modules/exec.cpp
@@ -3,9 +3,21 @@
 #include <cstring>
 #include <stdexcept>
 #include <unistd.h>
+#include <sys/capability.h>
+#include <sys/prctl.h>
 
 void ExecModule::apply(Runner& runner) {
     runner.addHook(Hook::Exec, [&](pid_t) {
+        // drop capabilities
+        cap_t cap = cap_get_proc();
+        if (!cap) {
+            throw std::runtime_error{"Failed to get capabilities"};
+        }
+        cap_clear(cap);
+        cap_set_proc(cap);
+        cap_free(cap);
+        prctl(PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0);
+
         std::vector<char*> argvp;
         for (auto& s : argv) argvp.push_back(s.data());
         argvp.push_back(nullptr);
